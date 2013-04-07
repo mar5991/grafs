9a0c01beaca1b32a2b942eb64e3a0888e4e36e96
marcin <marcin@marcin.(none)>
2013-04-07 22:46:48 +0200

    nowazmiana

--- grafs.cpp
+++ grafs.cpp
@@ -1,10 +1,13 @@
+#include <algorithm>
 #include <vector>
+#include <iostream>
+#include <string>
 #include <cstddef>
 #include <set>
+#include <map>
 using namespace std;
 class wierzcholek;
 class krawedz;
-
 class przegladaniekrawedzi
 {
 	public:
@@ -28,6 +31,12 @@ class techdata
 	virtual bool odw()
 	{
 	}
+	virtual void setodl(int wart)
+	{
+	}
+	virtual int odl()
+	{
+	}
 };
 class graf
 {
@@ -42,26 +51,8 @@ class graf
 	{
 		wierzcholki.insert(wi);
 	}
-	void przegladajkrawedzie(przegladaniekrawedzi p)
-	{
-		set <krawedz*>::iterator it1;
-		it1=krawedzie.begin();
-		while(it1!=krawedzie.end())
-		{
-			p.przegladaj(*it1);
-			it1++;
-		}
-	}
-	void przegladajwierzcholki(przegladaniewierzcholkow w)
-	{	
-		set <wierzcholek*>::iterator it1;
-		it1=wierzcholki.begin();
-		while(it1!=wierzcholki.end())
-		{
-			w.przegladaj(*it1);
-			it1++;
-		}
-	}
+	void przegladajkrawedzie(przegladaniekrawedzi *p);
+	void przegladajwierzcholki(przegladaniewierzcholkow *w);
 };
 
 class krawedz
@@ -69,6 +60,7 @@ class krawedz
 	wierzcholek* start;
 	wierzcholek* stop;
 	graf* g;
+	map <string, int> properities;
 	bool dwukier;
 	public:
 	techdata* danetech;
@@ -105,10 +97,19 @@ class krawedz
 			return true;
 		return false;
 	}
+	void setprop(string str, int wart)
+	{
+		properities[str]=wart;
+	}
+	int prop(string str)
+	{
+		return properities[str];
+	}
 };
 
 class wierzcholek
 {
+	map <string, int> properities;
 	vector <krawedz*> krawedzie;
 	graf* g;
 	public:
@@ -122,11 +123,23 @@ class wierzcholek
 	{
 		krawedzie.push_back(kraw);
 	}
-	void przegladajkrawedzie(przegladaniekrawedzi p)
+	void przegladajkrawedzie(przegladaniekrawedzi* p)
 	{
 		int s1=krawedzie.size();
 		for(int i=0; i<s1; i++)
-			p.przegladaj(krawedzie[i]);
+			p->przegladaj(krawedzie[i]);
+	}
+	void setprop(string str, int wart)
+	{
+		properities[str]=wart;
+	}
+	int prop(string str)
+	{
+		return properities[str];
+	}
+	graf* grafo()
+	{
+		return g;
 	}
 };
 krawedz::krawedz(graf* gr, wierzcholek* sta, wierzcholek* sto, bool dwukie) : g(gr)
@@ -139,6 +152,37 @@ krawedz::krawedz(graf* gr, wierzcholek* sta, wierzcholek* sto, bool dwukie) : g(
 	start->dodajkrawedz(this);
 	stop->dodajkrawedz(this);
 }
+class dfs
+{
+	public:
+	int numerpre;
+	int numerpost;
+	string preo;
+	string poso;
+	string odwi;
+	dfs(string pre, string pos, string odw)
+	{
+		preo=pre;
+		poso=pos;
+		odwi=odw;
+	}
+	virtual void preorder(wierzcholek* w, int wart)
+	{
+		if(preo!="")
+			w->setprop(preo, wart);
+	}
+	virtual void postorder(wierzcholek* w, int wart)
+	{	
+		if(preo!="")
+			w->setprop(poso, wart);
+	}
+	virtual void odwiedzone(krawedz* k)
+	{	
+		if(preo!="")
+			k->setprop(odwi, 1);
+	}
+	void init(graf* g);
+};
 class dfsdata : public techdata
 {
 	public:
@@ -156,12 +200,72 @@ class dfsdata : public techdata
 		od=0;
 	}
 };
+class dijkstra
+{
+	set<pair <int, wierzcholek*> > listaodl;
+	map <wierzcholek*, krawedz*> pary;
+	string odle;
+	string calkodle;
+	string odw;
+	public:
+	dijkstra (string odl, string codl, string odwe) : odle(odl), calkodle(codl), odw(odwe)
+	{
+	}
+	virtual int odleglosc(krawedz* k)
+	{
+		return k->prop(odle);
+	}
+	virtual void setodleglosc(wierzcholek* w, int wart)
+	{
+		w->setprop(calkodle, wart);
+	}
+	virtual void odwiedzone(krawedz* k)
+	{
+		k->setprop(odw, 1);
+	}
+	void init(wierzcholek* w);
+};
+class dijkstradata : public techdata
+{
+	public:
+	bool od;
+	int odleglosccalk;
+	dijkstradata()
+	{
+		od=0;
+		odleglosccalk=100000007;
+	}
+	void setodw(bool wart)
+	{
+		od=wart;
+	}
+	void setodl(int wart)
+	{
+		odleglosccalk=wart;
+	}
+	int odl()
+	{
+		return odleglosccalk;
+	}
+	bool odw()
+	{
+		return od;
+	}
+};
 class dfsfirstprzegl : public przegladaniewierzcholkow
 {
 	public:
 	void przegladaj(wierzcholek* w)
 	{
 		w->danetech=new dfsdata();
+	}
+};
+class dijkstrafirstprzegl : public przegladaniewierzcholkow
+{	
+	public:
+	void przegladaj(wierzcholek* w)
+	{
+		w->danetech=new dijkstradata();
 	}
 };
 class dfslastprzegl : public przegladaniewierzcholkow
@@ -172,21 +276,72 @@ class dfslastprzegl : public przegladaniewierzcholkow
 		delete w->danetech;
 	}
 };
+class dijkstralastprzegl : public przegladaniewierzcholkow
+{
+	dijkstra* dd;
+	public:
+	dijkstralastprzegl(dijkstra* d) : dd(d)
+	{
+	}
+	void przegladaj(wierzcholek* w)
+	{
+		dd->setodleglosc(w, w->danetech->odl());
+		delete w->danetech;
+	}
+};
 class dfsprzegladaniekrawedzi : public przegladaniekrawedzi
 {
 	wierzcholek* poprzedni;
+	dfs* df;
 	public:
-	dfsprzegladaniekrawedzi(wierzcholek* w)
+	dfsprzegladaniekrawedzi(wierzcholek* w, dfs* dfss) : df(dfss)
 	{
 		poprzedni=w;
 	}
 	void przegladaj(krawedz* k);
 };
-void properdfs(wierzcholek* w)
+class dijkstraprzegladaniekrawedzi : public przegladaniekrawedzi
+{
+	wierzcholek* ww;
+	dijkstra* dijk;
+	map <wierzcholek*, krawedz*> *pary;
+	set <pair <int, wierzcholek*> > *secik;
+	public:
+	dijkstraprzegladaniekrawedzi(wierzcholek* w, dijkstra* dij, set <pair <int, wierzcholek*> > *sec, map <wierzcholek*, krawedz*> *par) : ww(w), dijk(dij), secik(sec), pary(par)
+	{
+	}
+	void przegladaj(krawedz* k)
+	{
+		if(k->nastepny(ww)==NULL)
+			return;
+		if(!k->nastepny(ww)->danetech->odw())
+		{
+			int wart1=ww->danetech->odl();
+			int wart2=dijk->odleglosc(k);
+			pair<int, wierzcholek*> ppp(k->nastepny(ww)->danetech->odl(), k->nastepny(ww));
+			set <pair <int, wierzcholek*> >::iterator it1=secik->find(ppp);
+			if(it1!=secik->end())
+				secik->erase(it1);
+			pair<int, wierzcholek*> ppe(min(k->nastepny(ww)->danetech->odl(), wart1+wart2), k->nastepny(ww));
+			if(k->nastepny(ww)->danetech->odl()>=wart1+wart2)
+			{
+				(*pary)[k->nastepny(ww)]=k;
+			}
+			k->nastepny(ww)->danetech->setodl(min(k->nastepny(ww)->danetech->odl(), wart1+wart2));
+			secik->insert(ppe);
+		}
+	}
+};
+void properdfs(wierzcholek* w, dfs* df)
 {
 	w->danetech->setodw(1);
-	dfsprzegladaniekrawedzi dpk(w);
-	w->przegladajkrawedzie(dpk);
+	dfsprzegladaniekrawedzi dpk(w, df);
+	df->preorder(w, (df->numerpre));
+	(df->numerpre)++;
+	w->przegladajkrawedzie(&dpk);
+	df->postorder(w, (df->numerpost));
+	(df->numerpost)++;
+
 }
 void dfsprzegladaniekrawedzi::przegladaj(krawedz* k)
 {
@@ -195,28 +350,101 @@ void dfsprzegladaniekrawedzi::przegladaj(krawedz* k)
 		return;
 	if(nast->danetech->odw())
 		return;
-	properdfs(nast);
+	df->odwiedzone(k);
+	properdfs(nast, df);
 }
 class dfssrodkoweprzegl : public przegladaniewierzcholkow
 {
+	dfs* df;
 	public:
+	dfssrodkoweprzegl(dfs* dfss) : df(dfss)
+	{
+	}
 	void przegladaj(wierzcholek* w)
 	{
-		properdfs(w);
+		if(!w->danetech->odw())
+			properdfs(w, df);
 	}
 };
-class dfs
+void dfs::init(graf* g)
 {
-	void init(graf* g)
+	numerpre=0;
+	numerpost=0;
+	dfsfirstprzegl dfp;
+	dfslastprzegl dlp;
+	dfssrodkoweprzegl dsp(this);
+	g->przegladajwierzcholki(&dfp);
+	g->przegladajwierzcholki(&dsp);
+	g->przegladajwierzcholki(&dlp);
+}
+void dijkstra::init(wierzcholek* w)
+{
+	dijkstrafirstprzegl dfp;
+	w->grafo()->przegladajwierzcholki(&dfp);
+	pair <int, wierzcholek*> para(0, w);
+	w->danetech->setodl(0);
+	listaodl.insert(para);
+	while(!listaodl.empty())
 	{
-		dfsfirstprzegl dfp;
-		dfslastprzegl dlp;
-		dfssrodkoweprzegl dsp;
-		g->przegladajwierzcholki(dfp);
-		g->przegladajwierzcholki(dsp);
-		g->przegladajwierzcholki(dlp);
+		pair<int, wierzcholek*> parx=*(listaodl.begin());
+		listaodl.erase(parx);
+		(parx.second)->danetech->setodw(1);
+		dijkstraprzegladaniekrawedzi dpk(parx.second, this, &listaodl, &pary);
+		(parx.second)->przegladajkrawedzie(&dpk);
 	}
-};
+	map<wierzcholek*, krawedz*>::iterator it1=pary.begin();
+	while(it1!=pary.end())
+	{
+		odwiedzone(it1->second);
+		it1++;
+	}
+	dijkstralastprzegl dlp(this);
+	w->grafo()->przegladajwierzcholki(&dlp);
+}
+void graf::przegladajkrawedzie(przegladaniekrawedzi* p)
+{
+	set <krawedz*>::iterator it1;
+	it1=krawedzie.begin();
+	while(it1!=krawedzie.end())
+	{
+		p->przegladaj(*it1);
+		it1++;
+	}
+}
+void graf::przegladajwierzcholki(przegladaniewierzcholkow* w)
+{	
+	set <wierzcholek*>::iterator it1;
+	it1=wierzcholki.begin();
+	while(it1!=wierzcholki.end())
+	{
+		w->przegladaj(*it1);
+		it1++;
+	}
+}
 int main()
 {
+	int n, m;
+	cin>>n>>m;
+	graf g;
+	wierzcholek* w[n];
+	krawedz* k[m];
+	for(int i=0; i<n; i++)
+	{
+		w[i]=new wierzcholek(&g);
+	}
+	for(int i=0; i<m; i++)
+	{
+		int x, y, z;
+		cin>>x>>y>>z;
+		x--;
+		y--;
+		k[i]=new krawedz(&g, w[x], w[y], 1);
+		k[i]->setprop("odl", z);
+	}
+	dijkstra dij("odl", "kodl", "odw");
+	dij.init(w[0]);
+	for(int i=0; i<n; i++)
+		cout<<w[i]->prop("kodl")<<endl;
+	for(int i=0; i<m; i++)
+		cout<<"*"<<k[i]->prop("odw")<<endl;
 }
